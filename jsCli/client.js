89d6280447f6c6c3c47ab2cd66de0d9e87a7a688
srlopez <you@example.com>
2021-02-05 11:48:05 +0100

    Js socket

---
+++ jsCli/client.js
@@ -0,0 +1,17 @@
+var net = require('net');
+
+var client = new net.Socket()
+
+//conexion cliente
+port=7654;
+ip = '127.0.0.1' 
+client.connect(port, ip, function() {
+    console.log('Conectado');
+    client.write('{ "action":"LOGIN", "value":"SOKETJS" }')
+});
+client.on("data", (data) => {
+    console.log(data.toString());
+});
+client.on('end', () => {
+    console.log('disconnected from server');
+});
